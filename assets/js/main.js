eb2a66da1295a3ab6e4f90d0624cff014a94c3f6
10xMike <michelegec@gmail.com>
2021-06-08 11:24:08 +0200

    Commit iniziale

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/*
+Milestone 1:
+Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: titolo, titolo originale, lingua, voto.
+*/
+
+const app = new Vue({
+    el: '#app',
+    data:{
+
+    },
+    methods:{},
+    mounted(){
+
+    },
+});
